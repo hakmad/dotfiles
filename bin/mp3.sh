1940734bf1360f95c477e278ed6b2bd12c43261f
hakmad <84286331+hakmad@users.noreply.github.com>
2021-06-20 17:51:13 +0100

    Added extra line to mp3.sh to create directories if they don't already exist

--- bin/mp3.sh
+++ bin/mp3.sh
@@ -20,10 +20,14 @@ youtube-dl --extract-audio --audio-format mp3\
 
 # If on desktop...
 if [[ $OSTYPE == "linux-gnu" ]]; then
+	# Create directory if it doesn't already exist.
+	mkdir -p ~/media/music
 	# Move file to ~/media/music.
 	mv "$FILENAME".mp3 ~/media/music
 # If on mobile (Android).
 elif [[ $OSTYPE == "linux-android" ]]; then
+	# Create directory if it doesn't already exist.
+	mkdir -p ~/storage/music
 	# Move file to ~/storage/music.
 	mv "$FILENAME".mp3 ~/storage/music
 fi
