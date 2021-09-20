1d87b9e4ff8346f42f973a714a041f3cf83e96ea
hakmad <88987013+hakmad@users.noreply.github.com>
2021-09-20 21:01:37 +0100

    Added push-dotfiles.sh, a script for linking or copying dotfiles to specified locations

---
+++ bin/push-dotfiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+NAME=$1
+ORIGIN=~/.dotfiles/$NAME
+
+if [[ -d $ORIGIN ]] && [[ -f $ORIGIN/.location ]]; then
+	TARGET=$(cat $ORIGIN/.location)
+else
+	echo "Necessary files/folders not found, exiting"
+	exit 1
+fi
+
+push() {
+	local ORIGIN=$1
+	local TARGET=$2
+
+	mkdir -p $TARGET
+
+	for item in $(ls $ORIGIN); do
+		if [[ -d $ORIGIN$item ]]; then
+			push $ORIGIN$item/ $TARGET$item/
+		else
+			if ln -s -f $ORIGIN$item $TARGET$item ; then
+				echo "Linked $ORIGIN$item to $TARGET$item"
+			elif sudo cp $ORIGIN$item $TARGET$item ; then
+				echo "Copied $ORIGIN$item to $TARGET$item"
+			else
+				echo "Failed to push $ORIGIN$item to \
+$TARGET$item"
+			fi
+		fi
+	done
+}
+
+push $ORIGIN $TARGET
+
+echo "Pushed dotfiles for $NAME"
