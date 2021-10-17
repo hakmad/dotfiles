f15bdb0ce6a62dac4a1f9237480178ee90fa2caa
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-17 19:33:13 +0100

    Added comments to backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# Script for creating a backup of the following directories:
+#	~/.dotfiles/
+# 	~/downloads/
+# 	~/media/
+# 	~/workspace/
+# 
+# The usage of this script is as follows:
+# 	backup.sh -v, --verbose		Enable verbosity.
+# 	backup.sh -o, --output [file]	Write to [file].
+#
+# where [file] is the name of the file to write to.
+#
+# The resulting file is a tarball compressed with gzip.
+
+# Basic variables.
 VERBOSE=false
 BACKUP_DIR=~/.backup
 FILENAME="backup.tar.gz"
 
+# Working directory of the user.
 WORKING_DIR=$(pwd)
 
+# Get command line arguments.
 while [[ $1 != "" ]]; do
 	case $1 in
 		-v | --verbose)
@@ -23,6 +40,7 @@ while [[ $1 != "" ]]; do
 	esac
 done
 
+# Verbosity functions.
 if [[ $VERBOSE == true ]]; then
 	log() {
 		echo "$@"
@@ -33,17 +51,20 @@ else
 	}
 fi
 
+# Create temporary directory for backup.
 log "Creating backup directory..."
 rm -rf $BACKUP_DIR
 mkdir -p $BACKUP_DIR
 cd $BACKUP_DIR
 
+# Copy directories.
 log "Copying files..."
 cp -r ~/.dotfiles ./dotfiles
 cp -r ~/downloads .
 cp -r ~/media .
 cp -r ~/workspace .
 
+# Create tarball.
 log "Creating $FILENAME..."
 if [[ $VERBOSE == true ]]; then
 	tar -czvf $FILENAME *
@@ -51,9 +72,11 @@ else
 	tar -czf $FILENAME *
 fi
 
+# Move resulting file to home directory.
 log "Moving $FILENAME to home directory..."
 mv $FILENAME ~/
 
+# Clean up.
 log "Cleaning up..."
 rm -rf $BACKUP_DIR
 cd $WORKING_DIR
