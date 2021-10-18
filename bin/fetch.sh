48416d29c350ad9d64dd3007c1a24eb798a775c8
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-18 19:47:42 +0100

    Added comments to fetch.sh

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Script for showing system information.
+
 clear
 
+# Colours.
 C="\033[1;37m"
 NC="\033[0m"
 
+# Get information.
 source /etc/os-release
 
 CPU=$(lscpu | sed -nr "/Model name/ s/.*:\s*(.*) @ .*/\1/p")
@@ -19,6 +23,7 @@ TOTAL_DISK=$(df -h | grep $ROOT_DISK | awk '{print $2}')
 BATTERY_STATUS=$(acpi --battery | cut -d "," -f 1 | cut -d " " -f 3)
 BATTERY_REMAINING=$(acpi --battery | cut -d " " -f 5)
 
+# Show information.
 echo -e "$C \b$USER$NC@$C \b$HOSTNAME$NC\n"
 echo -e "$C \bOS:$NC $NAME"
 echo -e "$C \bCPU:$NC $CPU"
