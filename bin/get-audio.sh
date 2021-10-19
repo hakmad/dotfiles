2a5a53684396b7067596d93f1959b68c5f7ef2b3
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-19 17:53:09 +0100

    Added comments to get-audio.sh

--- bin/get-audio.sh
+++ bin/get-audio.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
+# Script for outputting audio information.
+# 
+# The usage of this script is as follows:
+# 	get-audio.sh -a, --all		Get all information.
+# 	get-audio.sh -v, --volume	Get volume information.
+# 	get-audio.sh -s, --status	Get status information.
+
+# Get volume.
 return_volume() {
 	VOLUME=$(amixer get Master | tail -n 1 | cut -d "[" -f 2 | tr -d "]")
 	echo $VOLUME
 }
 
+# Get status.
 return_status() {
 	STATUS=$(amixer get Master | tail -n 1 | cut -d "[" -f 3 | tr -d "]")
 	echo $STATUS
 }
 
+# Output volume.
 output_volume() {
 	echo "Audio at $(return_volume)"
 }
 
+# Output status.
 output_status() {
 	echo "Audio is $(return_status)"
 }
 
+# Output everything.
 output_all() {
 	echo "Audio is $(return_status), at $(return_volume)"
 }
 
+# Get command line arguments.
 while [[ $1 != "" ]]; do
 	case $1 in
 		-a | --all)
