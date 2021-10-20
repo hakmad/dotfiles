b5d33c698dca0aa8fe0c3cc2abaeef56962c4f43
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-20 19:34:57 +0100

    Added comments to get-backlight.sh

--- bin/get-backlight.sh
+++ bin/get-backlight.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Script for outputting backlight information.
+# 
+# The usage of this script is as follows:
+# 	get-backlight.sh
+
 BACKLIGHT=$(xbacklight -get | cut -d "." -f 1)
 echo "Backlight is at $BACKLIGHT%"
