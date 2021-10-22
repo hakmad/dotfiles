9d13e9981504f0e347850b7c6d8528a513e30eae
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-22 22:08:12 +0100

    Added comments to get-battery.sh

--- bin/get-battery.sh
+++ bin/get-battery.sh
@@ -1,36 +1,52 @@
 #!/bin/bash
 
+# Script for outputting battery information.
+# 
+# The usage of this script is as follows:
+# 	get-battery.sh -a, --all	Get all information.
+# 	get-battery.sh -p, --percentage	Get battery percentage.
+# 	get-battery.sh -t, --time	Get time remaining.
+# 	get-battery.sh -s, --status	Get status infomation.
+
+# Get percentage.
 return_percentage() {
 	PERCENTAGE=$(acpi | cut -d " " -f 4 | tr -d ",")
 	echo $PERCENTAGE
 }
 
+# Get time.
 return_time() {
 	TIME=$(acpi | cut -d "," -f 3)
 	echo $TIME
 }
 
+# Get status.
 return_status() {
 	STATUS=$(acpi | cut -d " " -f 3 | tr -d ",")
 	echo $STATUS
 }
 
+# Output percentage.
 output_percentage() {
 	echo $(return_percentage)
 }
 
+# Output time.
 output_time() {
 	echo "$(return_time)"
 }
 
+# Output status.
 output_status() {
 	echo $(return_status)
 }
 
+# Output everything.
 output_all() {
 	echo "$(return_status), $(return_time) ($(return_percentage))"
 }
 
+# Get command line arguments.
 while [[ $1 != "" ]]; do
 	case $1 in
 		-a | --all)
