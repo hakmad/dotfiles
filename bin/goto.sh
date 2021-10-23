463a48b06cc663e9aa0b31ab332a83d254c5a7d9
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-23 17:58:12 +0100

    Added comments to goto.sh

--- bin/goto.sh
+++ bin/goto.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Script for going to a desktop.
+# 
+# Usage of this script is as follows:
+# 	goto.sh [desktop] - go to [desktop].
+# 
+# where [desktop] is the name of the desktop to move to.
+
 bspc desktop -f $@
 popup.sh -w 100 -m " Workspace ${@#?}"
