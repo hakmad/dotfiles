92d050c3479ab007eb4089594d4715e066070de6
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-23 18:13:21 +0100

    Added comments to move.sh

--- bin/move.sh
+++ bin/move.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Script for moving a node to a desktop.
+# 
+# Usage of this script is as follows:
+# 	move.sh [desktop] - move currently focused node to [desktop].
+# 
+# where [desktop] is the name of the desktop to move to.
+
 bspc node -d "$@"
 popup.sh -w 150 -m " Moved to workspace ${@#?}"
