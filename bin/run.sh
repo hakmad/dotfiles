7a616b3507c9d1e5961a92e5099a458f88c749a7
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-24 19:39:11 +0100

    Added comments to run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Script for running desktop applications.
+
+# Get colours and fonts.
 source style.sh
 
+# List of options.
 OPTIONS="alacritty\ndiscord\nfirefox\nobs\nqutebrowser\nvirtualbox"
 
+# Run dmenu with options and run selected option in the shell.
 echo -e $OPTIONS | dmenu -fn $FONT -nb $BACKGROUND -nf $ACCENT \
 	-sb  $BACKGROUND -sf $FOREGROUND \
 	-b -x $X -y $Y -w $WIDTH -h $HEIGHT -s 0 | $SHELL &
