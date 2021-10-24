1d3e77af4765b207c411542c76235d5447c44056
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-24 19:32:26 +0100

    Added comments to modules.sh

--- bin/modules.sh
+++ bin/modules.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# Script for opening notes for a specific module.
+
+# Get colours and fonts.
 source style.sh
 
+# Basic variables.
 MODULES_LOCATION=~/workspace/notes/university-modules/
 MODULES_LIST_FULLPATH=$(find $MODULES_LOCATION -mindepth 2 -maxdepth 2 -type d)
 MODULES_LIST=""
 
+# Add modules to list for dmenu.
 for module in $MODULES_LIST_FULLPATH; do
 	module=$(echo $module | sed "s:.*/::")
 	MODULE_LIST="$MODULE_LIST$module\n"
 done
 
+# Run dmenu and get choice.
 CHOICE=$(echo -e $MODULE_LIST | dmenu -fn $FONT -nb $BACKGROUND -nf \
 	$ACCENT -sb $BACKGROUND -sf $FOREGROUND \
 	-b -x $X -y $Y -w $WIDTH -h $HEIGHT -s 0)
 
+# Open notes.
 if [[ -z $CHOICE ]]; then
 	exit
 else
