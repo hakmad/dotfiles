f3f86c395b07b799489e3e1c8afe4b9982cc89b0
hakmad <88987013+hakmad@users.noreply.github.com>
2021-10-26 18:57:43 +0100

    Added comments to popup.sh

--- bin/popup.sh
+++ bin/popup.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Script for creating desktop popups with lemonbar.
+# 
+# The usage of this script is as follows:
+# 	popup.sh -d [duration] -w [width] -m [message]
+# 
+# where:
+# 	[duration] is the time (in seconds) for the popup to last
+# 	(defaults to 1).
+# 	[width] is the width (in pixels) for the popup.
+# 	[message] is the message to be shown.
+
+# Get colours and fonts.
 source style.sh
 
+# Basic variables.
 DURATION=1
 
+# Get command line arguments.
 while [[ $1 != "" ]]; do
 	case $1 in
 		-w)
@@ -22,8 +36,7 @@ while [[ $1 != "" ]]; do
 	shift
 done
 
-
-
+# Run popup.
 (echo " $MESSAGE"; sleep $DURATION) | lemonbar -d -b \
 	-g ${WIDTH}x${HEIGHT}+${X}+${Y} \
 	-F $FOREGROUND -B $BACKGROUND -f "$FONT"
