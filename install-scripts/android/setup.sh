4ece7cb82585b6f64f842b19ced0e1a1801cc6f0
hakmad <88987013+hakmad@users.noreply.github.com>
2021-11-03 19:09:43 +0000

    Added setup.sh, a script for setting up Android (Termux)

---
+++ install-scripts/android/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setup script for Android (Termux).
+
+# Set up Termux storage.
+termux-setup-storage
+
+# Update packages and refresh package database.
+pkg upgrade
+
+# Install youtube-dl through pip.
+pip install --upgrade pip
+pip install youtube-dl
+
+# Clone dotfiles.
+git clone https://github.com/hakmad/dotfiles ~/.dotfiles
+
+# Push dotfiles.
+~/.dotfiles/bin/push-dotfiles.sh bash
+~/.dotfiles/bin/push-dotfiles.sh bin
+~/.dotfiles/bin/push-dotfiles.sh git
+~/.dotfiles/bin/push-dotfiles.sh vim
