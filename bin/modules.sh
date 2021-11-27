4fbc3ab1a9311c2fe123b714ac1ce6eed674b3c9
hakmad <88987013+hakmad@users.noreply.github.com>
2021-11-27 23:56:14 +0000

    Restructured modules.sh a bit

--- bin/modules.sh
+++ bin/modules.sh
@@ -6,13 +6,13 @@
 source style.sh
 
 # Basic variables.
-MODULES_LOCATION=~/workspace/notes/university-modules/
-MODULES_LIST_FULLPATH=$(find $MODULES_LOCATION -mindepth 2 -maxdepth 2 -type d)
+MODULES_LOCATION=~/workspace/university/
+MODULES_LIST_FULLPATH=$(find $MODULES_LOCATION* -maxdepth 0 -type d -not -name template)
 MODULES_LIST=""
 
 # Add modules to list for dmenu.
 for module in $MODULES_LIST_FULLPATH; do
-	module=$(echo $module | sed "s:.*/::")
+	module=$(echo $module | cut -d "/" -f 6)
 	MODULE_LIST="$MODULE_LIST$module\n"
 done
 
@@ -26,7 +26,7 @@ if [[ -z $CHOICE ]]; then
 	exit
 else
 	RESULT=$(find $MODULES_LOCATION -type d -name $CHOICE)
-	cd $RESULT
+	cd $RESULT/notes
 	alacritty --command vim notes.tex &
 	sleep 0.1
 	zathura --fork notes.pdf &
