9df34af74f2d354abd9034e93159b39d4809713e
hakmad <88987013+hakmad@users.noreply.github.com>
2022-01-12 12:57:09 +0000

    Removed android-studio from run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -6,7 +6,7 @@
 source style.sh
 
 # List of options.
-OPTIONS="alacritty\nandroid-studio\ndiscord\nfirefox\nobs\npavucontrol\
+OPTIONS="alacritty\ndiscord\nfirefox\nobs\npavucontrol\
 	\nqutebrowser\nvia-ui\nvirtualbox\nwireshark"
 
 # Run dmenu with options and run selected option in the shell.
