dafa21d2997aa8e8ae3d778a5f249b06ea7a61b8
hakmad <88987013+hakmad@users.noreply.github.com>
2022-02-05 12:47:01 +0000

    Removed Discord and VS Code from packages to install in setup-laptop.sh

--- install-scripts/arch/setup-laptop.sh
+++ install-scripts/arch/setup-laptop.sh
@@ -68,7 +68,7 @@ install_aur lemonbar-xft-git
 # Install desktop applications.
 log "Installing desktop applications..."
 sudo pacman -S --noconfirm alacritty qutebrowser pavucontrol firefox \
-        zathura zathura-pdf-mupdf discord mpv feh obs-studio code wireshark-qt
+        zathura zathura-pdf-mupdf mpv feh obs-studio wireshark-qt
 
 # Install miscellaneous utilities.
 log "Installing miscellaneous utilities..."
