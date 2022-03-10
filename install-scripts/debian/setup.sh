fb40c5323c9559484235f45eaf37befd3a499740
hakmad <88987013+hakmad@users.noreply.github.com>
2022-03-10 15:57:20 +0000

    Changed setup.sh for Debian to be run by root instead of the regular user and added lines to create a new user

--- install-scripts/debian/setup.sh
+++ install-scripts/debian/setup.sh
@@ -15,25 +15,33 @@ set -e
 rm -f setup.log
 log "Setup started!"
 
+# Setup users.
+log "Setting password for root..."
+passwd
+
+log "Adding new user..."
+useradd -m -G sudo -s /usr/bin/bash hakmad
+passwd hakmad
+
 # Update packages, refresh package database.
 log "Updating..."
-sudo apt update
-sudo apt upgrade
+apt update
+apt upgrade
 
 # Install basic and miscellaneous utilities.
 log "Installing basic and miscellaneous utilities..."
-sudo apt install vim git man-db tree htop python zip unzip p7zip jq \
+apt install vim git man-db tree htop python zip unzip p7zip jq \
 	bash-completion
 
 # Clone dotfiles onto new system and push dotfiles.
 log "Cloning dotfiles..."
-git clone https://github.com/hakmad/dotfiles ~/.dotfiles
+git clone https://github.com/hakmad/dotfiles /home/hakmad/.dotfiles
 
 log "Pushing dotfiles..."
-~/.dotfiles/bin/push-dotfiles.sh bash
-~/.dotfiles/bin/push-dotfiles.sh bin
-~/.dotfiles/bin/push-dotfiles.sh vim
-~/.dotfiles/bin/push-dotfiles.sh git
+su hakmad -c /home/hakmad/.dotfiles/bin/push-dotfiles.sh bash
+su hakmad -c /home/hakmad/.dotfiles/bin/push-dotfiles.sh bin
+su hakmad -c /home/hakmad/.dotfiles/bin/push-dotfiles.sh vim
+su hakmad -c /home/hakmad/.dotfiles/bin/push-dotfiles.sh git
 
 # Setup complete.
 log "Setup complete! Please login as hakmad. :)"
