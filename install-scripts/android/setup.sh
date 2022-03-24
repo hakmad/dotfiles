b02c6f4b16a6301011072fc36d559084edb947a5
hakmad <88987013+hakmad@users.noreply.github.com>
2022-03-24 15:48:26 +0000

    Added logging functionality to setup.sh for Android (Termux)

--- install-scripts/android/setup.sh
+++ install-scripts/android/setup.sh
@@ -2,24 +2,41 @@
 
 # Setup script for Android (Termux).
 
+# Function for logging.
+log() {
+	echo $@
+	echo $(date "+%Y/%m/%d %H:%M:%S") $@ >> setup.log
+}
+
+# Remove old log file.
+rm -f setup.log
+log "Setup started!"
+
 # Set up Termux storage.
+log "Setting up storage..."
 termux-setup-storage
 
 # Update packages and refresh package database.
+log "Updating..."
 pkg upgrade
 
 # Install programs.
+log "Installing utilities..."
 pkg install ffmpeg git python vim man
 
 # Install youtube-dl through pip.
+log "Installing youtube-dl"
 pip install --upgrade pip
 pip install youtube-dl
 
-# Clone dotfiles.
+# Clone and push dotfiles.
+log "Setting up dotfiles..."
 git clone https://github.com/hakmad/dotfiles ~/.dotfiles
 
-# Push dotfiles.
 ~/.dotfiles/bin/push-dotfiles.sh bash
 ~/.dotfiles/bin/push-dotfiles.sh bin
 ~/.dotfiles/bin/push-dotfiles.sh git
 ~/.dotfiles/bin/push-dotfiles.sh vim
+
+# Setup complete.
+log "Setup complete! Please restart Termux. :)"
