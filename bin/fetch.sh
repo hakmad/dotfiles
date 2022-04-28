dd0cd949c15b234846eac074828f4e5d14d49608
hakmad <88987013+hakmad@users.noreply.github.com>
2022-04-28 09:12:11 +0100

    Added uptime information to fetch.sh

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -24,12 +24,15 @@ TOTAL_DISK=$(df -h | grep $ROOT_DISK | awk '{print $2}')
 BATTERY_STATUS=$(acpi --battery | cut -d "," -f 1 | cut -d " " -f 3)
 BATTERY_REMAINING=$(acpi --battery | cut -d " " -f 5)
 
+UPTIME="$(uptime -p | sed "s/up //") (since $(uptime -s))"
+
 # Show information.
 echo -e "$C \b$USER$NC@$C \b$HOSTNAME$NC\n"
 echo -e "$C \bOS:$NC $NAME"
 echo -e "$C \bCPU:$NC $CPU"
 echo -e "$C \bRAM:$NC $USED_RAM / $TOTAL_RAM"
 echo -e "$C \bDisk:$NC $USED_DISK / $TOTAL_DISK"
+echo -e "$C \bUptime:$NC $UPTIME"
 if [[ -z $BATTERY_REMAINING ]]; then
 	:	
 else
