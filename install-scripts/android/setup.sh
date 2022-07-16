997b54e3204030d0681a045e2b313f8381bcac69
hakmad <hakmad@fastmail.com>
2022-07-16 19:04:12 +0100

    Removed logging from setup.sh

--- install-scripts/android/setup.sh
+++ install-scripts/android/setup.sh
@@ -2,35 +2,20 @@
 
 # Setup script for Android (Termux).
 
-# Function for logging.
-log() {
-	echo $@
-	echo $(date "+%Y/%m/%d %H:%M:%S") $@ >> setup.log
-}
-
-# Remove old log file.
-rm -f setup.log
-log "Setup started!"
-
 # Set up Termux storage.
-log "Setting up storage..."
 termux-setup-storage
 
 # Update packages and refresh package database.
-log "Updating..."
 pkg upgrade
 
 # Install programs.
-log "Installing utilities..."
 pkg install ffmpeg git python vim man openssh
 
 # Install youtube-dl through pip.
-log "Installing youtube-dl"
 pip install --upgrade pip
 pip install youtube-dl
 
 # Clone and push dotfiles.
-log "Setting up dotfiles..."
 git clone https://github.com/hakmad/dotfiles ~/.dotfiles
 
 ~/.dotfiles/bin/push-dotfiles.sh bash
@@ -39,4 +24,4 @@ git clone https://github.com/hakmad/dotfiles ~/.dotfiles
 ~/.dotfiles/bin/push-dotfiles.sh vim
 
 # Setup complete.
-log "Setup complete! Please restart Termux. :)"
+echo "Setup complete. Please restart Termux!"
