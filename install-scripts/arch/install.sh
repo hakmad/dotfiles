8a4503465910111f12dcfcc801b3755270bd1a44
hakmad <hakmad@fastmail.com>
2022-07-16 19:05:21 +0100

    Removed logging from install.sh

--- install-scripts/arch/install.sh
+++ install-scripts/arch/install.sh
@@ -2,80 +2,56 @@
 
 # Script for installing Arch Linux.
 
-# Basic variables.
+# System variables.
 HOSTNAME="laptop"
 USER="hakmad"
-
-# Logging function.
-log () {
-	echo $@
-	echo $(date "+%Y/%m/%d %H:%M%:%S") $@ >> install.log
-}
 
 # Stop the script if there are errors.
 set -e
 
-# Remove old log file.
-rm -f install.log
-log "Installation started!"
-
 # Load UK keyboard.
-log "Loading UK keyboard..."
 loadkeys uk
 
 # Use NTP.
-log "Setting NTP..."
 timedatectl set-ntp true
 
 # Partition and format /dev/sda.
-log "Partitioning /dev/sda..."
 fdisk /dev/sda
 ESP=$(fdisk /dev/sda -l | awk '/EFI/ {print $1}')
 ROOT=$(fdisk /dev/sda -l | awk '/Linux/ {print $1}')
-log "Formatting root partition ($ROOT) as ext4..."
 mkfs.ext4 $ROOT
 if [[ $ROOT == "/dev/sda2" ]]; then
-	log "Formatting boot partition ($ESP) as FAT32..."
 	mkfs.fat -F 32 $ESP
-else
-	log "Not formatting boot partition ($ESP), as FAT32, leaving untouched..."
 fi
 
 # Mount partitions.
-log "Mounting partitions..."
 mount $ROOT /mnt
 mkdir /mnt/boot
 mount $ESP /mnt/boot
 
 # Get mirrorlist.
-log "Setting up mirrorlist..."
 curl -L https://www.archlinux.org/mirrorlist/?country=GB > \
 	/etc/pacman.d/mirrorlist
 sed -i "s/^#Server/Server/" /etc/pacman.d/mirrorlist
 
 # Install Arch base onto mountpoint.
-log "Installing Arch base..."
 pacstrap /mnt base base-devel linux linux-firmware vi vim git man-pages \
 	man-db networkmanager
 
 # Generate fstab file.
-log "Generating fstab file..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Set time zone.
-log "Setting timezone..."
 arch-chroot /mnt ln -sf /usr/share/Europe/London /etc/localtime
 
 # Generate locales.
 sed -i "s/^#en_US.UTF-8/en_US.UTF-8/" /mnt/etc/locale.gen
 sed -i "s/^#en_GB.UTF-8/en_GB.UTF-8/" /mnt/etc/locale.gen
-log "Generating locales..."
 arch-chroot /mnt locale-gen
 
 # Set locales, keyboards and fonts.
 echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
 echo "KEYMAP=uk" >> /mnt/etc/vconsole.conf
-echo "FONT=ter-112n" >> /mnt/etc/vconsole.conf
 
 # Set hostname and hosts.
 echo $HOSTNAME >> /mnt/etc/hostname
@@ -83,10 +59,10 @@ echo -e "127.0.0.1\tlocalhost" >> /mnt/etc/hosts
 echo -e "::1\tlocalhost" >> /mnt/etc/hosts
 
 # Setup users.
-log "Setting password for root..."
+echo "Setting password for root user"
 arch-chroot /mnt passwd
 arch-chroot /mnt useradd -m -G wheel $USER
-log "Setting password for $USER..."
+echo "Setting password for user $USER"
 arch-chroot /mnt passwd $USER
 arch-chroot /mnt visudo
 
@@ -96,7 +72,6 @@ arch-chroot /mnt git clone https://github.com/$USER/dotfiles \
 arch-chroot /mnt chown -R $USER:$USER /home/$USER/.dotfiles
 
 # Install (temporary) bootloader.
-log "Installing bootloader..."
 arch-chroot /mnt bootctl install --path=/boot
 echo -e "default\tarch.conf" > /mnt/boot/loader/loader.conf
 rm -f /mnt/boot/loader/entries/arch.conf
@@ -106,4 +81,4 @@ echo -e "initrd\t/initramfs-linux.img" >> /mnt/boot/loader/entries/arch.conf
 echo -e "options\troot=$ROOT rw" >> /mnt/boot/loader/entries/arch.conf
 
 # Installation complete.
-log "Installation complete! Please reboot. :)"
+echo "Setup complete. Please reboot!"
