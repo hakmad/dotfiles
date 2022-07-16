95d92c6d8f2ae553e683d6909f6ceea7aee07e7b
hakmad <hakmad@fastmail.com>
2022-07-16 18:45:36 +0100

    Removed logging function from setup.sh

--- install-scripts/arch/setup.sh
+++ install-scripts/arch/setup.sh
@@ -11,77 +11,54 @@ install_aur() {
 	rm -rf $1
 }
 
-# Function for logging.
-log () {
-	echo $@
-	echo $(date "+%Y/%m/%d %H:%M:%S") $@ >> setup.log
-}
-
 # Stop the script if there are errors.
 set -e
 
-# Remove old log file.
-rm -f setup.log
-log "Setup started!"
-
 # Connect to the internet.
-log "Starting NetworkManager service..."
 sudo systemctl enable --now NetworkManager.service
 sleep 5
-log "Connecting to the internet..."
 sudo nmtui
 sleep 5
 
 # Update packages, refresh package database.
-log "Updating..."
 sudo pacman -Syu --noconfirm
 
 # Set time zone.
-log "Setting timezone..."
 sudo timedatectl set-timezone Europe/London
 
 # Install X and video drivers.
-log "Installing X and video drivers..."
 sudo pacman -S --noconfirm xorg xorg-xinit xf86-video-intel xf86-video-fbdev \
 	mesa
 
 # Install audio and setup alsa.
-log "Installing audio packages..."
 sudo pacman -S --noconfirm alsa-utils alsa-lib pulseaudio \
 	pulseaudio-alsa
 set +e
-log "Starting alsa..."
 sudo alsactl init
 set -e
 
 # Install fonts.
-log "Installing fonts..."
 sudo pacman -S --noconfirm noto-fonts noto-fonts-extra noto-fonts-emoji \
 	noto-fonts-cjk gnu-free-fonts ttf-liberation
-
-install_aur cozette-otb
 install_aur bdf2psf
+install_aur cozette-otb
 install_aur psf-cozette
 
 # Install desktop utilities.
-log "Installing desktop utilities..."
 sudo pacman -S --noconfirm bspwm sxhkd picom scrot slock
 install_aur dmenu2
 install_aur lemonbar-xft-git
 
 # Install desktop applications.
-log "Installing desktop applications..."
 sudo pacman -S --noconfirm alacritty qutebrowser firefox \
 	zathura zathura-pdf-mupdf feh keepassxc syncthing
 
 # Install miscellaneous utilities.
-log "Installing miscellaneous utilities..."
 sudo pacman -S --noconfirm acpi tree ntfs-3g htop wireless_tools python \
 	python-pip texlive-most youtube-dl zip unzip p7zip jq \
 	xss-lock bash-completion xsel xdotool xclip openssh
 
 # Push dotfiles.
-log "Pushing dotfiles..."
 cd ~/.dotfiles/
 for package in $(find * -maxdepth 0 -type d -not \( \
 	-name install-scripts -or
@@ -92,7 +69,6 @@ cd -
 
 # Extra things.
 # Remove fsck hooks.
-log "Regenerating initial RAM-disk..."
 sudo mkinitcpio -p linux
 
 # Set qutebrowser as the default browser.
@@ -100,6 +76,3 @@ xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
 
 # Generate SSH keys for this machine.
 ssh-keygen
-
-# Setup complete.
-log "Setup complete! Please reboot. :)"
