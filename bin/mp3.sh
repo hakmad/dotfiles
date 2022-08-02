42a08c689c3b543a72f9bec793f81942c2abd750
hakmad <hakmad@fastmail.com>
2022-08-02 18:58:09 +0100

    Changed how mp3.sh downloads music - uses read instead now

--- bin/mp3.sh
+++ bin/mp3.sh
@@ -3,39 +3,39 @@
 # Script to download MP3 files with youtube-dl and move them to a location.
 # 
 # The usage of this script is as follows:
-# 	mp3.sh [URL] [artist] [title]
-# 
-# where [URL] is the URL and [artist]/[title] are the relevant tags to apply.
-# The file is saved as [artist]-[title].mp3.
+# 	mp3.sh
 
 # Stop the script if there are errors.
 set -e
 
-# Basic variables.
-URL=$1
-ARTIST=$2
-TITLE=$3
-FILENAME=$ARTIST-$TITLE
+# Use `read` to get input variables.
+read -rp "URL: " URL
+read -rp "Artist: " ARTIST
+read -rp "Title: " TITLE
+read -rp "Genre(s): " GENRES
+
+# Construct filename.
+FILENAME="$ARTIST-$TITLE"
+echo "Saving file as $FILENAME.mp3"
 
 # Download file.
-yt-dlp --extract-audio --audio-format mp3 \
-	--output "$FILENAME".%\(ext\)s $URL \
-	--postprocessor-args "-metadata artist=\"$ARTIST\" -metadata title=\"$TITLE\""
+yt-dlp --extract-audio --audio-format mp3 --output "$FILENAME".%\(ext\)s "$URL" \
+	--postprocessor-args "ffmpeg:-metadata artist=\"$ARTIST\" -metadata title=\"$TITLE\" -metadata genre=\"$GENRES\""
 
 # Find music directory.
 if [[ -z $ANDROID_ROOT ]]; then
-	DOWNLOADS_DIR=~/media/music/
+	DOWNLOADS_DIR="$HOME/media/music/"
 else
-	DOWNLOADS_DIR=~/storage/music/
+	DOWNLOADS_DIR="$HOME/storage/music/"
 fi
 
 # Move file to music directory.
-mkdir -p $DOWNLOADS_DIR
-mv "$FILENAME".mp3 $DOWNLOADS_DIR
+mkdir -p "$DOWNLOADS_DIR"
+mv "$FILENAME.mp3" "$DOWNLOADS_DIR"
 
 # On Android, ask MediaStore to rescan music directory.
 if [[ -a $ANDROID_ROOT ]]; then
-	termux-media-scan $DOWNLOADS_DIR
+	termux-media-scan "$DOWNLOADS_DIR"
 fi
 
 echo "Successfully downloaded $FILENAME using youtube-dl"
