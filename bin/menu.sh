45ffd6cc700de34bbfe064ab469476d971986b51
hakmad <88987013+hakmad@users.noreply.github.com>
2022-10-19 19:36:08 +0100

    Added menu.sh, a script for creating menus with dmenu

---
+++ bin/menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script for creating menues with dmenu.
+# 
+# The usage of this script is as follows:
+# 	[options] | menu.sh | [handler]
+# 
+# where:
+# 	[options] is the different options to be passed to dmenu.
+# 	[handler] is whatever is responsible for handling the option
+# 	selected by the user (usually Bash, though it is possible to
+# 	not have a handler at all).
+
+# Get colours and fonts.
+source style.sh
+
+cat - | dmenu -fn $FONT -nb $BACKGROUND -nf $ACCENT \
+	-sb $BACKGROUND -sf $FOREGROUND -b -x $X -y $Y \
+	-w $WIDTH -h $HEIGHT
