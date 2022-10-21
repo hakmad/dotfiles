5829c6cbfd6b1a2210e468a39d38bb6ba6b7d20e
hakmad <88987013+hakmad@users.noreply.github.com>
2022-10-21 15:37:49 +0100

    Updated run.sh to use menu.sh

--- bin/run.sh
+++ bin/run.sh
@@ -2,9 +2,6 @@
 
 # Script for running desktop applications.
 
-# Get colours and fonts.
-source style.sh
-
 # List of options.
 OPTIONS="alacritty
 firefox
@@ -18,6 +15,4 @@ via-ui
 wireshark"
 
 # Run dmenu with options and run selected option in the shell.
-echo -e "$OPTIONS" | dmenu -fn $FONT -nb $BACKGROUND -nf $ACCENT \
-	-sb  $BACKGROUND -sf $FOREGROUND \
-	-b -x $X -y $Y -w $WIDTH -h $HEIGHT -s 0 -q | $SHELL &
+echo -e "$OPTIONS" | menu.sh | $SHELL &
