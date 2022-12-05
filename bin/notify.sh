fe5443cdec39b98d12eabd7b0564fcb09ff5f01a
hakmad <hakmad@fastmail.com>
2022-12-05 14:17:23 +0000

    Added -y flag for popups to notify.sh

--- bin/notify.sh
+++ bin/notify.sh
@@ -10,11 +10,11 @@ battery() {
 	STATUS=$(battery.sh status)
 
 	if [[ $PERCENTAGE == 100 ]] && [[ $STATUS == "Full" ]]; then
-		popup.sh -d 3 -m "Unplug laptop - Battery full" &
+		popup.sh -d 3 -m "Unplug laptop - Battery full" -y 50 &
 	elif [[ $PERCENTAGE -ge 90 ]] && [[ $STATUS == "Charging" ]]; then
-		popup.sh -d 3 -m "Unplug laptop - Battery high" &
+		popup.sh -d 3 -m "Unplug laptop - Battery high" -y 50 &
 	elif [[ $PERCENTAGE -lt 10 ]] && [[ $STATUS == "Discharging" ]]; then
-		popup.sh 3 -m "Plug in laptop - Battery low" &
+		popup.sh 3 -m "Plug in laptop - Battery low" -y 50 &
 	fi
 }
 
@@ -24,7 +24,7 @@ network() {
 	PREVIOUS=$(cat /tmp/network)
 
 	if [[ "$CURRENT" != "$PREVIOUS" ]]; then
-		popup.sh -d 3 -m "$(get-network.sh)" &
+		popup.sh -d 3 -m "$(get-network.sh)" -y 50 &
 
 		echo "$CURRENT" > /tmp/network
 	fi
