681806b41929cbb1bdc772773195ad432fb08b07
hakmad <hakmad@fastmail.com>
2022-12-09 12:07:23 +0000

    Removed get-backlight.sh

--- bin/get-backlight.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script for outputting backlight information.
-# 
-# The usage of this script is as follows:
-# 	get-backlight.sh
-
-BACKLIGHT=$(light -G | cut -d "." -f 1)
-echo "Backlight is at $BACKLIGHT%"
