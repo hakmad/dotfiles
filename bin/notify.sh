0ca39f318b06b361223db6281b45ee93b6a1bfb0
hakmad <hakmad@fastmail.com>
2022-12-13 14:58:21 +0000

    Added longer popup times for notifications

--- bin/notify.sh
+++ bin/notify.sh
@@ -10,11 +10,11 @@ battery() {
 	STATUS=$(battery.sh status)
 
 	if [[ $PERCENTAGE == 100 ]] && [[ $STATUS == "Full" ]]; then
-		popup.sh -d 3 -m "Unplug laptop - Battery full" -y 50 &
+		popup.sh -d 5 -m "Unplug laptop - Battery full" -y 50 &
 	elif [[ $PERCENTAGE -ge 90 ]] && [[ $STATUS == "Charging" ]]; then
-		popup.sh -d 3 -m "Unplug laptop - Battery high" -y 50 &
+		popup.sh -d 5 -m "Unplug laptop - Battery high" -y 50 &
 	elif [[ $PERCENTAGE -lt 10 ]] && [[ $STATUS == "Discharging" ]]; then
-		popup.sh 3 -m "Plug in laptop - Battery low" -y 50 &
+		popup.sh -d 5 -m "Plug in laptop - Battery low" -y 50 &
 	fi
 }
 
