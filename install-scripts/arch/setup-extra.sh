3286f0f1137e4bb0789c159279defbe9042bcbb7
hakmad <hakmad@fastmail.com>
2023-08-19 11:07:35 +0100

    Removed steam from list of applications

--- install-scripts/arch/setup-extra.sh
+++ install-scripts/arch/setup-extra.sh
@@ -3,7 +3,7 @@
 # Extra setup script for Arch Linux.
 
 # Install extra desktop applications.
-sudo pacman -S --noconfirm steam obs-studio shotcut krita blender mpv
+sudo pacman -S --noconfirm obs-studio shotcut krita blender mpv
 
 # Install TeX live.
 curl -L -O https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
