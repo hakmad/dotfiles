fbe5ec189ee0e6c07612cf65eff040befb7cea34
hakmad <hakmad@fastmail.com>
2025-01-19 15:28:34 +0000

    Added script to synchronise todo notes and also added systemd service/timer units

---
+++ bin/sync-todo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set variables for local and cloud todo locations.
+local_todo="$HOME/todo/"
+cloud_todo="fastmail:todo"
+
+# Download file from the cloud (skip if the local version is newer than the
+# cloud version).
+rclone sync $cloud_todo $local_todo --update
+
+# Upload local file to the the cloud (skip if the cloud version is newer than
+# the local version).
+rclone sync $local_todo $cloud_todo --update
