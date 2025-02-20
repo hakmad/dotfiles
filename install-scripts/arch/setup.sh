e2b5ed7a32a0dbdca2c966a0397b3d15bc7e721a
hakmad <hakmad@fastmail.com>
2025-02-20 13:27:46 +0000

    Added some new packages, removed older ones and reworded comments

--- install-scripts/arch/setup.sh
+++ install-scripts/arch/setup.sh
@@ -61,20 +61,21 @@ install_aur dmenu2
 install_aur lemonbar-xft-git
 
 # Install desktop applications.
-sudo pacman -S --noconfirm alacritty qutebrowser firefox zathura zathura-pdf-mupdf feh mpv keepassxc
+sudo pacman -S --noconfirm alacritty qutebrowser firefox zathura zathura-pdf-mupdf feh mpv keepassxc vlc
 
 # Install miscellaneous utilities.
-sudo pacman -S --noconfirm tree ntfs-3g htop wireless_tools yt-dlp jq bash-completion xdotool xclip openssh zip unzip pz7ip android-tools mediainfo brightnessctl
+sudo pacman -S --noconfirm tree ntfs-3g htop wireless_tools yt-dlp jq bash-completion xclip openssh zip unzip pz7ip mediainfo brightnessctl
 
 # Install programming languages.
 sudo pacman -S --noconfirm python python-pip go
 
 # Extra things.
+
+# Regenerate initramfs.
+sudo mkinitcpio -p linux
+
 # Add user to light group.
 sudo usermod -a -G video $USER
-
-# Remove fsck hooks.
-sudo mkinitcpio -p linux
 
 # Generate SSH keys for this machine.
 ssh-keygen
