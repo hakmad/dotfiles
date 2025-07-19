d0114128b0cc1dabc05b120a74552457c93ea563
hakmad <hakmad@fastmail.com>
2025-07-19 22:08:25 +0100

    Added verbose option to sync and backup option
    The backup option should help me keep a local copy of my password file
    on my laptop just in case I lose the copy stored in the WebDAV file
    share.

--- bin/sync-passwords.sh
+++ bin/sync-passwords.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 # Set variables for local and cloud password locations.
+passwords_file="passwords.kdbx"
 local_passwords="$HOME/.passwords/"
 cloud_passwords="fastmail:passwords"
+backup_dir="$HOME/.backups/passwords/$(date -I)/"
+
+# Check if a backup has been made today.
+if [[ ! -f $backup_dir$passwords_file ]]; then
+    # Create a new backup directory and copy the passwords file there.
+    mkdir -p $backup_dir
+    cp $local_passwords$passwords_file $backup_dir
+
+    echo "Backed up $local_passwords$passwords_file to $backup_dir$passwords_file"
+fi
+
 
 # Download file from the cloud (skip if the local version is newer than the
 # cloud version).
-rclone sync $cloud_passwords $local_passwords --update
+rclone sync $cloud_passwords $local_passwords --update -v
 
 # Upload local file to the the cloud (skip if the cloud version is newer than
 # the local version).
-rclone sync $local_passwords $cloud_passwords --update
+rclone sync $local_passwords $cloud_passwords --update -v
