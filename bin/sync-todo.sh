d367b905f7cc0e4a185d2a7e02d00a71bd8ad84f
hakmad <hakmad@fastmail.com>
2025-07-19 22:09:32 +0100

    Added verbose option

--- bin/sync-todo.sh
+++ bin/sync-todo.sh
@@ -6,8 +6,8 @@ cloud_todo="fastmail:todo"
 
 # Download file from the cloud (skip if the local version is newer than the
 # cloud version).
-rclone sync $cloud_todo $local_todo --update
+rclone sync $cloud_todo $local_todo --update -v
 
 # Upload local file to the the cloud (skip if the cloud version is newer than
 # the local version).
-rclone sync $local_todo $cloud_todo --update
+rclone sync $local_todo $cloud_todo --update -v
