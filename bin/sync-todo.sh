975bbbcfb83658c994259dfc8327e7cafa0321f3
hakmad <hakmad@fastmail.com>
2025-10-23 19:30:27 +0100

    Modified backup location and function for todo

--- bin/sync-todo.sh
+++ bin/sync-todo.sh
@@ -3,7 +3,19 @@
 # Set variables for local and cloud todo locations.
 local_todo="$HOME/todo/"
 cloud_todo="fastmail:todo"
-local_backup="$HOME/.backups/todo/$(date -I)/"
+backup_dir="$HOME/media/backups/todo/$(date -I)/"
+
+# Make a backup.
+# I know that rclone has a backup option but I prefer doing this myself.
+# (Also the backup option doesn't always seem to work...)
+# Check if a backup has been made today.
+if [[ ! -f $backup_dir$todo_file ]]; then
+    # Create a new backup directory and copy the todo file there.
+    mkdir -p $backup_dir
+    cp -r $local_todo/. $backup_dir
+
+    echo "Backed up $local_todo to $backup_dir"
+fi
 
 # Run a bidirectional sync using rclone. Check below for explanations:
 # `--compare modtime` - comparisons are made based on modification time only.
@@ -11,8 +23,6 @@ local_backup="$HOME/.backups/todo/$(date -I)/"
 #   being deleted above a certain percent. I don't actually care about that since
 #   no files will be deleted from either side, so `--force` is need to make the sync
 #   happen.
-# `--backup-dir1 $local_backup` - path to back files up to on this side (my laptop).
 # `--conflict-resolve older` - if there is a conflict, choose the file that is older.
-# `--create-empty-src-dirs` - create directories if they don't already exist.
 # `--verbose` - please say what you are actually doing.
-rclone bisync $local_todo $cloud_todo --compare modtime --force --backup-dir1 $local_backup --conflict-resolve older --create-empty-src-dirs --verbose
+rclone bisync $local_todo $cloud_todo --compare modtime --force --conflict-resolve older --verbose
